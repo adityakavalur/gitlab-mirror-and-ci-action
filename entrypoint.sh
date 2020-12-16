11a642a57b9d1bb2d52f673f389bd4a807964096
adityakavalur <33232772+adityakavalur@users.noreply.github.com>
2020-12-16 15:26:21 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,8 +24,8 @@ POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 #Aditya: PR modifications
 echo $GITHUB_HEAD_REF
 
-echo "github.event $github.event"
-echo "github.event_name $github.event_name"
+echo "github.event $github_event"
+echo "github.event_name $github_event_name"
 
 git checkout "${GITHUB_HEAD_REF}"
 echo "Hello World1!"
