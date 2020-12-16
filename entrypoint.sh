7c4398f1ef1528d89908a613fe1c1983b50a6f8d
adityakavalur <33232772+adityakavalur@users.noreply.github.com>
2020-12-16 11:06:36 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,8 @@ sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
 sh -c "git remote add mirror $*"
 sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
-sh -c "git push mirror $branch --force"
+#sh -c "git push mirror $branch --force"
+sh -c "git push mirror $branch"
 
 sleep $POLL_TIMEOUT
 
