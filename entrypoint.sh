45bec19f70ba806edcacc52f5338829e8ed37001
adityakavalur <33232772+adityakavalur@users.noreply.github.com>
2021-02-02 22:14:46 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,9 @@ fi
 echo "entrypoint: line49"
 #list of pre-approved commiters based on whether repo is in username space or organization
 touch /dev/null > /tmp/github_usernames
-
+pwd
+ls -la /tmp
+ls -la .
 org_type=$(curl -H "Authorization: token ${GITHUB_TOKEN}" --silent -H "Accept: application/vnd.github.antiope-preview+json" https://api.github.com/repos/${GITHUB_REPOSITORY} | grep type | head -n 1 | awk '{print $2}' | sed "s/\\\"/\\,/g" | sed s/\[,\]//g)
 if [ "${org_type}" = "Organization" ]
 then
