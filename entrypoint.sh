f44c62401c4ad29b113d83e76e3607bfb0cf2616
adityakavalur <33232772+adityakavalur@users.noreply.github.com>
2021-03-01 17:27:37 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -171,8 +171,9 @@ then
    # Cycle through all PRs 
    for ipr in {1..${npr}}
    do
-      echo "line 174"
-      target_PR_NUMBER=$(curl --silent -H "Authorization: token ${SOURCE_PAT}" -H "Accept: application/vnd.github.antiope-preview+json" https://api.github.com/repos/${GITHUB_REPO}/pulls | jq ".[$(($ipr - 1))] | {PR_NUMBER : .number}" | jq .PR_NUMBER)
+      echo "line 174: $ipr"
+      localpr=$(($ipr-1))
+      target_PR_NUMBER=$(curl --silent -H "Authorization: token ${SOURCE_PAT}" -H "Accept: application/vnd.github.antiope-preview+json" https://api.github.com/repos/${GITHUB_REPO}/pulls | jq ".[$localpr] | {PR_NUMBER : .number}" | jq .PR_NUMBER)
       echo "${target_PR_NUMBER}"
       #Approvaltime is used to find the latest approved action, that PR will be targeted by CI.
       #This function only returns PRs where the latest commit is approved. 
